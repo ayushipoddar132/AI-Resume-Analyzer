1f49a3e4bb577360ed6d471f1249174b5bd5bc8a
ayushipoddar132 <ayushipoddar132@gmail.com>
2026-07-19 23:15:56 +0530

    Add resume constants and type definitions

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,6 @@
 import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
-
+import {resumes} from "../../Constants";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -10,13 +10,22 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-     <Navbar />
-    <section className="main-setion">
-      <div className="page-heading">
-        <h1>Track Your Applications & Resume Ratings</h1>
-        <h2>Review your submissions and check AI-powered feedback.</h2>
-      </div>
-    </section>
-  </main>
+  return (
+      <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+        <Navbar />
+
+        <section className="main-section">
+          <div className="page-heading">
+            <h1>Track Your Applications & Resume Ratings</h1>
+            <h2>Review your submissions and check AI-powered feedback.</h2>
+          </div>
+        </section>
+
+        {resumes.map((resume) => (
+            <div>
+              <h1>{resume.jobTitle}</h1>
+            </div>
+        ))}
+      </main>
+  );
 }
