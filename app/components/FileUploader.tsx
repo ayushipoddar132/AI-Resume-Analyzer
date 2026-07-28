7d65a2beaac8647a647db271c59ddd743bca8ea8
ayushipoddar132 <ayushipoddar132@gmail.com>
2026-07-28 22:26:10 +0530

    Update file uploader layout

--- app/components/FileUploader.tsx
+++ app/components/FileUploader.tsx
@@ -29,21 +29,26 @@ const FileUploader = ({ onFileSelect } : FileUploaderProps) => {
             <div {...getRootProps()}>
                 <input {...getInputProps()} />
                <div className="space-y-4 cursor-pointer">
-                   <div className="mx-auto w-16 h-16 flex items-center justify-center">
-                       <img src="/icons/info.svg" alt="upload" className="size-20"/>
-                   </div>
                    {file ? (
                        <div className="uploader-selected-file" onClick={(e) => e.stopPropagation()} >
-                       <div className="flex items-center space-x-3">
                            <img src="/images/pdf.png" alt="pdf" className="size-10" />
+                       <div className="flex items-center space-x-3">
                            <div>
                            <p className="text-sm text-gray-700 font-medium truncate max-w-xs">{file.name}</p>
                            <p className="text-sm text-gray-500">{formatSize(file.size)}</p>
                        </div>
                        </div>
+                           <button className="p-2 cursor-pointer" onClick={(e) => {
+                               onFileSelect?.(null)
+                           }}>
+                              <img src="/icons/cross.svg" alt="remove" className="w-4 h-4"/>
+                           </button>
                        </div>
                    ) : (
-                       <div>
+                       <div  className="flex flex-col items-center justify-center text-center h-full">
+                           <div className="mx-auto w-16 h-16 flex items-center justify-center mb-2">
+                               <img src="/icons/info.svg" alt="upload" className="size-20"/>
+                           </div>
                          <p className="text-lg text-gray-500">
                              <span className="font-semibold">
                                  Click to upload
