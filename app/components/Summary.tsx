97b408b8440952ce98adff18907ffb5198797e1e
ayushipoddar132 <ayushipoddar132@gmail.com>
2026-07-30 00:29:39 +0530

    Building Resume Feedback Components

--- app/components/Summary.tsx
+++ app/components/Summary.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import ScoreGauge from "~/components/ScoreGauge";
 
-const Summary = () => {
+const Category = ({ title, score } : {})
+
+const Summary = ({feedback} :{feedback: Feedback}) => {
     return (
-        <div>Summary</div>
+        <div className="bg-white rounded-2xl shadow-md w-full">
+            <div className="flex flex-row items-center p-4 gap-8">
+                <ScoreGauge score={feedback.overallScore} />
+                <div className="flex flex-col gap-2">
+                    <h2 className="text-2xl font-bold">Your Resume Score</h2>
+                    <p className="text-sm text-gray-500">
+                        This Score is calculated based on the variables listed below.
+                    </p>
+                </div>
+            </div>
+        </div>
     )
 }
 
