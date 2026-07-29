27c9ecaf415135425b64b2d22bbc28efc11a765f
ayushipoddar132 <ayushipoddar132@gmail.com>
2026-07-30 01:59:22 +0530

    Modified summary

--- app/components/Summary.tsx
+++ app/components/Summary.tsx
@@ -1,6 +1,22 @@
 import ScoreGauge from "~/components/ScoreGauge";
 
-const Category = ({ title, score } : {})
+const Category = ({ title, score } : {title: string, score: number}) => {
+    const textColor = score >= 70 ? "text-green-600" : score > 49 ? "text-yellow-600" : "text-red-600";
+
+    return (
+        <div className="resume-summary">
+            <div className="category">
+                <div className="flex flex-row gap-2 items-center justify-center">
+                   <p className="text-2xl">{title}</p>
+
+                </div>
+                <p className="text-2xl">
+                    <span className={textColor}>{score}</span>/100
+                </p>
+            </div>
+        </div>
+    )
+}
 
 const Summary = ({feedback} :{feedback: Feedback}) => {
     return (
@@ -14,6 +30,10 @@ const Summary = ({feedback} :{feedback: Feedback}) => {
                     </p>
                 </div>
             </div>
+            <Category title="Tone & Style" score ={feedback.toneAndStyle.score} />
+            <Category title="Content" score ={feedback.content.score} />
+            <Category title="Structure" score ={feedback.structure.score} />
+            <Category title="Skills" score ={feedback.skills.score} />
         </div>
     )
 }
