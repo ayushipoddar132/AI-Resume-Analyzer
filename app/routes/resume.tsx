ae3cb93d0e4c67204f67ec380149d64b5e8b9d8e
ayushipoddar132 <ayushipoddar132@gmail.com>
2026-07-29 23:43:37 +0530

    modified resume.tsx

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -9,9 +9,9 @@ export const meta = () => ([
 const Resume = () => {
     const { auth, isLoading, fs, kv } = usePuterStore();
     const { id } = useParams();
-    const [imageUrl, setImageUrl] = useState();
-    const [resumeUrl, setResumeUrl] = useState();
-    const [feedback, setFeedback] = useState();
+    const [imageUrl, setImageUrl] = useState('');
+    const [resumeUrl, setResumeUrl] = useState('');
+    const [feedback, setFeedback] = useState<Feedback | null>(null);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -35,6 +35,7 @@ const Resume = () => {
             setImageUrl(imageUrl);
 
             setFeedback(data.feedback);
+            console.log({resumeUrl, imageUrl, feedback: data.feedback});
         }
 
         loadResume();
@@ -48,10 +49,10 @@ const Resume = () => {
                 </Link>
             </nav>
             <div className="flex flex-row w-full max-lg:flex-col-reverse">
-                <section className="feedback-section bg-[url('/images/bg-small.svg') bg-cover h-[100vh] sticky top-0 items-center justify-center">
+                <section className="feedback-section bg-[url('/images/bg-small.svg')] bg-cover h-[100vh] sticky top-0 items-center justify-center">
                     {imageUrl && resumeUrl && (
                         <div className="animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-wxl:h-fit w-fit">
-                         <a>
+                         <a href={resumeUrl} target="_blank" rel="noopener noreferrer">
                              <img
                                  src={imageUrl}
                                  className="w-full h-full object-contain rounded-2xl"
